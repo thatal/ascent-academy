599e752802ad7386af0b9db1935a844ce1626474
rupamjbordoloi <rupam@webcomipl.net.in>
2019-09-10 10:54:30 +0530

    ba 3rd sem open

--- config/constants.php
+++ config/constants.php
@@ -10,8 +10,8 @@ return [
 	'admission_up_time' => '2019-08-16 12:59:59',
     'admission_down_time' => '2019-08-29 23:59:59',
     'admission_course' => [2],
-    'admission_stream' => [4,5,6,7,8,9],
-    'admission_semester' => [5,7],
+    'admission_stream' => [6],
+    'admission_semester' => [5],
 
 	'helpline_no' => '9706533599',
 	'helpline_mail' => 'darrangcollegeadmission@gmail.com',
