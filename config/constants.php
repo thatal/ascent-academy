e83ae4a7fddc247360e1ced4d0e7aafedda95e7b
rupamjbordoloi <rupam@webcomipl.net.in>
2019-09-25 13:19:50 +0530

    admission

--- config/constants.php
+++ config/constants.php
@@ -1,36 +1,36 @@
 <?php
 return [
-	'current_time' => time(),
-	'apply_up_time' => '2019-09-17 11:00:00',
-    'apply_down_time' => '2019-09-17 23:59:59',
-    'apply_course' => [1],
-    'apply_stream' => [1,2,3],
-    'apply_semester' => [1],
+    'current_time'             => time(),
+    'apply_up_time'            => '2019-09-17 11:00:00',
+    'apply_down_time'          => '2019-09-17 23:59:59',
+    'apply_course'             => [1],
+    'apply_stream'             => [1, 2, 3],
+    'apply_semester'           => [1],
 
-	'admission_up_time' => '2019-09-24 09:59:59',
-    'admission_down_time' => '2019-09-25 23:59:59',
-    'admission_course' => [1,2],
-    'admission_stream' => [1,2,3,8,9],
-    'admission_semester' => [2,5],
+    'admission_up_time'        => '2019-09-24 09:59:59',
+    'admission_down_time'      => '2019-09-25 23:59:59',
+    'admission_course'         => [1, 2],
+    'admission_stream'         => [1, 2, 3, 8, 9],
+    'admission_semester'       => [2, 3, 5],
 
-	'helpline_no' => '9706533599',
-	'helpline_mail' => 'darrangcollegeadmission@gmail.com',
+    'helpline_no'              => '9706533599',
+    'helpline_mail'            => 'darrangcollegeadmission@gmail.com',
 
-	'application_fee' => 250,
-	'application_fee_word' => 'Two Hundred Fifty',
+    'application_fee'          => 250,
+    'application_fee_word'     => 'Two Hundred Fifty',
 
-	// sms gateway
-	'sms_user' => 'college',
-	'sms_password' => '8f420c8066XX',
-	'sms_senderid' => 'DRGCLG',
-	'sms_url' => 'http://t.instaclicksms.in/sendsms.jsp',
+    // sms gateway
+    'sms_user'                 => 'college',
+    'sms_password'             => '8f420c8066XX',
+    'sms_senderid'             => 'DRGCLG',
+    'sms_url'                  => 'http://t.instaclicksms.in/sendsms.jsp',
 
     // payment gateway
-    'url' => 'https://pgi.billdesk.com/pgidsk/PGIMerchantPayment',
-	'redirect_url_application' => 'http://68.183.87.189/darrang-college/payment-response',
-	// 'redirect_url_admission' => 'http://localhost/darrang-college/admission/payment-response',
-	'redirect_url_admission' => 'http://68.183.87.189/darrang-college/admission/payment-response',
-	'merchant_id' => 'DARRANGCOL',
-    'security_id' => 'darrangcol',
-    'checksum_key' => 'Wkbpw4zoFB5tQJq5A257isEejDqGXnYL'
+    'url'                      => 'https://pgi.billdesk.com/pgidsk/PGIMerchantPayment',
+    'redirect_url_application' => 'http://68.183.87.189/darrang-college/payment-response',
+    // 'redirect_url_admission' => 'http://localhost/darrang-college/admission/payment-response',
+    'redirect_url_admission'   => 'http://68.183.87.189/darrang-college/admission/payment-response',
+    'merchant_id'              => 'DARRANGCOL',
+    'security_id'              => 'darrangcol',
+    'checksum_key'             => 'Wkbpw4zoFB5tQJq5A257isEejDqGXnYL',
 ];
