a672477180e119458652159f755bf1c3648b8c85
Sunil Thatal <sunilthatal007@gmail.com>
2019-10-26 17:48:05 +0530

    Exam practical in Response

--- app/Http/Controllers/Student/AdmissionController.php
+++ app/Http/Controllers/Student/AdmissionController.php
@@ -322,7 +322,7 @@ class AdmissionController extends Controller
 
                         $with_practical = false;
                         $stream_id      = $application->appliedStream->stream_id;
-                        $with_practical = $application->with_practical;
+                        $with_practical = $application->exam_practical;
 
                         $fees = $application->feeStructure($with_practical, $stream_id);
                         foreach($fees->feeStructures as $fee_key => $fee){
